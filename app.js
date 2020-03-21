e9075c5e445da773903671cbf172f3df9e8438d8
FRobaglia <felix.robaglia@gmail.com>
2020-03-21 18:33:19 +0100

    06 Servir un template pug

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const dotenv = require('dotenv').config();
 
 const app = express();
 
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-  res.send('04 Rafraichissement automatique');
+  res.render('index');
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
