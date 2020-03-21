a20626aed2c8d767dda56170d64ceed3e11521f4
FRobaglia <felix.robaglia@gmail.com>
2020-03-21 19:18:29 +0100

    12 POST vers l'API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,9 +12,13 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'Page d\'accueil' });
 });
 
+
+
 app.post('/party', function(req, res) {
-  console.log(req.body);
-  res.send('Post ok!');
+  axios
+    .post(`${process.env.API_URL}/party`, req.body)
+    .then(({data}) => console.log(data))
+    .catch((err) => console.error(err));
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
