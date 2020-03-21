40614a4d34705d3f251fcd0a6177202ad9ca29ba
FRobaglia <felix.robaglia@gmail.com>
2020-03-21 18:30:03 +0100

    04 Rafraichissement automatique

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('02 Le fichier app.js');
+  res.send('04 Rafraichissement automatique');
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
