6e9187aca5cee01b469b676094773a727290b8e8
FRobaglia <felix.robaglia@gmail.com>
2020-03-22 17:39:54 +0100

    27 Effectuer la copie

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -3,7 +3,21 @@ if (navigator.clipboard) {
     const $button = document.createElement('button');
     $button.innerHTML = 'Copier le lien';
     $clipboardEl.parentNode.append($button);
+    $button.addEventListener(
+      'click',
+      copyToClipboard.bind(this, $clipboardEl, $button)
+    );
   });
 } else {
   console.warn("Pas de support du presse-papier")
+}
+
+function copyToClipboard($clipboardEl, $button) {
+  navigator.clipboard
+  .writeText($clipboardEl.getAttribute('data-clipboard'))
+  .then(() => {
+    $button.innerHTML = 'Copié !';
+    setTimeout(() => ($button.innerHTML = 'Copier le lien'), 2000);
+  })
+  .catch((err) => console.warn(err));
 }
