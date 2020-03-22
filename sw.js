70f3c53a605c78c734df83de2450deef477967f8
FRobaglia <felix.robaglia@gmail.com>
2020-03-22 18:30:24 +0100

    33 Intercepter une requête

--- sw.js
+++ sw.js
@@ -4,3 +4,7 @@ console.log(`Hello ${name}`);
 addEventListener('install', (event) => {
   console.log('Hello from the service worker')
 });
+
+addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
